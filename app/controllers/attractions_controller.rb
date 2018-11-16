10b10b8c333f943117262a4087936dc58d9765e2
alyssa0528 <alyssa.kim@gmail.com>
2018-11-16 14:50:02 +0000

    add search route and create attractions controller

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,9 @@
+class Attractions < ApplicationController
+  
+  def search
+    query = params[:query]
+
+    url = "https://api.foursquare.com/v2/venues/search?near=#{query}&categoryId=4d4b7104d754a06370d81259"
+  end
+
+end
