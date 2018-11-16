52f2acaa51bae5d60338bafdc0d5e276294e6da9
alyssa0528 <alyssa.kim@gmail.com>
2018-11-16 16:09:14 +0000

    update route and add client ID and secret code

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -3,7 +3,13 @@ class AttractionsController < ApplicationController
   def search
     query = params[:query]
 
-    url = "https://api.foursquare.com/v2/venues/search?near=#{query}&categoryId=4d4b7104d754a06370d81259"
+    client_id = ENV["CLIENT_ID"]
+
+    client_secret = ENV["CLIENT_SECRET"]
+
+    url = "https://api.foursquare.com/v2/venues/search?near=#{query}&categoryId=4d4b7104d754a06370d81259&client_id=#{client_id}&client_secret=#{client_secret}&v=20160201"
+
+    render "Hello"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/search/:query', to: 'attractions#search'
+  get '/search/:query' => 'attractions#search'
 
 end
